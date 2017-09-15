0e0dd6ce6202aaf0630dfd396e4b934ddf2890d6
daniil_ <daniil_evsienko@mail.ru>
2017-09-15 14:21:19 +0400

    Fixed build script + added GUI debugging mode to Bochs

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 ".\third party\fasm\fasm" src\boot.asm .\bin\boot.bios.bin
 
 ".\third party\dd" if=.\bin\boot.bios.bin of=.\bin\boot_sector.bin bs=512 count=1
-".\third party\dd" if=.\bin\boot.bios.bin of=.\bin\boot.bin bs=1 skip=512
+".\third party\dd" if=.\bin\boot.bios.bin of=.\disk\boot.bin bs=1 skip=512
 
-.\src\tools\make_listfs of=.\bin\disk.img bs=512 size=2880 boot=.\bin\boot_sector.bin
+.\src\tools\make_listfs of=.\bin\disk.img bs=512 size=2880 boot=.\bin\boot_sector.bin src=.\disk
