3df70268af81ad005dcbe7f372a10a031b6e1373
daniil_ <daniil_evsienko@mail.ru>
2017-09-27 12:54:15 +0400

    Fixed building scripts

---
+++ clean.bat
@@ -0,0 +1,5 @@
+del bin\*.bin
+del bin\*.exe
+del disk\*.bin
+del disk\*.exe
+del disk\*.img
--- compile.bat
+++ compile.bat
@@ -1,3 +1,5 @@
+call clean.bat
+
 mingw32-make -C src
 copy /y src\boot\boot.bios.bin bin\
 copy /y src\kernel\kernel.bin bin\
@@ -6,10 +8,9 @@ copy /y src\make_listfs\make_listfs.exe bin\
 dd if=bin\boot.bios.bin of=bin\boot_sector.bin bs=512 count=1
 dd if=bin\boot.bios.bin of=disk\boot.bin bs=1 skip=512
 
-copy /y bin\kernel.bin disk\system\kernel.bin
+copy /y bin\kernel.bin disk\kernel.bin
 
 bin\make_listfs of=disk.img bs=512 size=2880 boot=bin\boot_sector.bin src=.\disk 
 move /y disk.img disk\disk.img
-copy /y src\boot.cfg disk\boot.cfg
 
 echo "Press Enter to continue..."
