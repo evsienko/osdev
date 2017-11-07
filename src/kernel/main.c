24501e84d8b2475f562f1f59450fe5ac7ec49153
daniil_ <daniil_evsienko@mail.ru>
2017-11-07 23:55:58 +0400

    Step 8: added tty driver (https://subscribe.ru/archive/comp.soft.myosdev/201207/22163422.html)

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,13 +1,14 @@
 #include "stdlib.h"
+#include "tty.h"
 
 typedef struct {
-	unsigned long long base;
-	unsigned long long size;
+	uint64 base;
+	uint64 size;
 } BootModuleInfo;
 
-void kernel_main(char boot_disk_id, void *memory_map, BootModuleInfo *boot_module_list) {
-	char *screen_buffer = (void*)0xB8000;
-	memset_word(screen_buffer, 0x0E00, 2000);
-	char msg[] = {'H',0x0E,'e',0x0E,'l',0x0E,'l',0x0E,'o',0x0E,' ',0x0E,'w',0x0E,'o',0x0E,'r',0x0E,'l',0x0E,'d',0x0E,'!',0x0E};
-	memcpy(screen_buffer, msg, sizeof(msg));
+void kernel_main(uint8 boot_disk_id, void *memory_map, BootModuleInfo *boot_module_list) {
+	init_tty();
+	set_text_attr(0x1F);
+	clear_screen();
+	out_string("Hello world!\n");
 }
