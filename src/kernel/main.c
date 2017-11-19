12d11d804b8ab3e3178382c62945c729841c1db7
daniil_ <daniil_evsienko@mail.ru>
2017-11-19 22:13:24 +0400

    Step 12: Added memory manager (https://subscribe.ru/archive/comp.soft.myosdev/201207/25070604.html)

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,4 +1,5 @@
 #include "stdlib.h"
+#include "memory_manager.h"
 #include "interrupts.h"
 #include "tty.h"
 
@@ -8,14 +9,12 @@ typedef struct {
 } BootModuleInfo;
 
 void kernel_main(uint8 boot_disk_id, void *memory_map, BootModuleInfo *boot_module_list) {
+	init_memory_manager(memory_map);
 	init_interrupts();
 	init_tty();
 	set_text_attr(15);
 	printf("Welcome to MyOS!\n");
-	while (true) {
-	 	char buffer[256];
-	 	out_string("Enter string: ");
-	 	in_string(buffer, sizeof(buffer));
-	 	printf("You typed: %s\n", buffer);
-	}
+	printf("kernel_page_dir = 0x%x\n", kernel_page_dir);
+	printf("memory_size = %d MB\n", memory_size / 1024 / 1024);
+    printf("get_page_info(kernel_page_dir, 0xB8000) = 0x%x\n", get_page_info(kernel_page_dir, (void*)0xB8000));
 }
