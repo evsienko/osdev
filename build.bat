f7740cbf2ae29be1cde14184dd37776ba3dfe28c
daniil_ <daniil_evsienko@mail.ru>
2018-04-06 01:54:09 +0400

    Added kernel loading

--- build.bat
+++ build.bat
@@ -4,5 +4,7 @@ del bin\KRNLDR.SYS
 
 nasm -f bin src\Stage1\Boot1.asm -o bin\Boot1.bin -i src\Stage1\
 nasm -f bin src\Stage2\Stage2.asm -o bin\KRNLDR.SYS -i src\Stage2\
+nasm -f bin src\Kernel\Stage3.asm -o bin\KRNL.SYS -i src\Kernel\
 
 tools\fat_imgen-2.2.4\fat_imgen.exe -c -s bin\Boot1.bin -i bin\KRNLDR.SYS -f floppy.img
+tools\fat_imgen-2.2.4\fat_imgen.exe -m -i bin\KRNL.SYS -f floppy.img
