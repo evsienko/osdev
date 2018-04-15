f161c7a3efed486d46350f2a44aea5c0698ce31c
daniil_ <daniil_evsienko@mail.ru>
2018-04-15 17:21:08 +0400

    Fixed build script

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@ del floppy.img
 del bin\Boot1.bin
 del bin\KRNLDR.SYS
 
-nasm -f bin src\Stage1\Boot1.asm -o bin\Boot1.bin -i src\Stage1\
-nasm -f bin src\Stage2\Stage2.asm -o bin\KRNLDR.SYS -i src\Stage2\
+nasm -f bin src\SysBoot\Stage1\Boot1.asm -o bin\Boot1.bin -i src\SysBoot\Stage1\
+nasm -f bin src\SysBoot\Stage2\Stage2.asm -o bin\KRNLDR.SYS -i src\SysBoot\Stage2\
 
 tools\fat_imgen-2.2.4\fat_imgen.exe -c -s bin\Boot1.bin -i bin\KRNLDR.SYS -f floppy.img
 tools\fat_imgen-2.2.4\fat_imgen.exe -m -i bin\KRNL32.EXE -f floppy.img
